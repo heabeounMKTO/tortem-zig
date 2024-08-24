54671bfe4a663299231482e0349822490e934b6d
AA <aa@AAs-MacBook-Pro.local>
2024-08-24 12:26:08 +0700

    red sphere

--- src/color.zig
+++ src/color.zig
@@ -5,29 +5,35 @@ const Ray = ray.Ray;
 const math = std.math;
 const print = std.io.getStdOut().writer();
 
-
 pub fn hitSphere(center: vec.Vec3, radius: f64, r: Ray) bool {
     const oc = center - r.origin;
-
-    return true;
+    const a = vec.dot_vector(r.direction, r.direction);
+    const b = -2.0 * vec.dot_vector(r.direction, oc);
+    const c = vec.dot_vector(oc, oc) - radius * radius;
+    const disc = b * b - 4 * a * c;
+    if (disc >= 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 /// returns color from where da ray touches
 pub fn rayColor(r: Ray) vec.Vec3 {
-    if (hitSphere(vec.Vec3{0.0, 0.0, -1.0}, 0.5, r)) {
-        return vec.Vec3{1.0, 0.0, 0.0};
+    if (hitSphere(vec.Vec3{ 0.0, 0.0, -1.0 }, 0.5, r)) {
+        return vec.Vec3{ 1.0, 0.0, 0.0 };
     } else {
-        const unit_direction: vec.Vec3 = vec.unit_vector_from_ray(r); 
+        const unit_direction: vec.Vec3 = vec.unit_vector_from_ray(r);
         const t = 0.5 * (unit_direction[1] + 1.0);
-        const ray_color = @as(vec.Vec3, @as(vec.Vec3, (@splat(1.0 - t)))) * vec.Vec3{1.0, 1.0, 1.0} + @as(vec.Vec3, @splat(t)) * vec.Vec3{0.5,0.6,1.0}; 
+        const ray_color = @as(vec.Vec3, @as(vec.Vec3, (@splat(1.0 - t)))) * vec.Vec3{ 1.0, 1.0, 1.0 } + @as(vec.Vec3, @splat(t)) * vec.Vec3{ 0.5, 0.6, 1.0 };
         return ray_color;
-    } 
+    }
 }
 
 /// normalizes to 0 -> 255 for values from 0.0-> 1.0
 pub fn pointToColor(point: vec.Point3) vec.Color {
-    const screen_r  = @as(i32, @intFromFloat(255.99 * point[0]));
-    const screen_g  = @as(i32, @intFromFloat(255.99 * point[1]));
-    const screen_b  = @as(i32, @intFromFloat(255.99 * point[2]));
-    return vec.Color {screen_r, screen_g, screen_b};
+    const screen_r = @as(i32, @intFromFloat(255.99 * point[0]));
+    const screen_g = @as(i32, @intFromFloat(255.99 * point[1]));
+    const screen_b = @as(i32, @intFromFloat(255.99 * point[2]));
+    return vec.Color{ screen_r, screen_g, screen_b };
 }
--- src/vec.zig
+++ src/vec.zig
@@ -1,6 +1,6 @@
 pub const Point3 = @Vector(3, f64);
 pub const Color = @Vector(3, i32);
-pub const Vec3 = @Vector(3,f64);
+pub const Vec3 = @Vector(3, f64);
 pub const ray = @import("ray.zig");
 const std = @import("std");
 const math = std.math;
@@ -30,9 +30,13 @@ pub fn vlen(comptime v: anytype) vtype(v) {
 pub fn unit_vector_from_ray(r: ray.Ray) Vec3 {
     return r.direction / @as(Vec3, @splat(math.sqrt(@reduce(.Add, r.direction * r.direction))));
 }
-
-
-
 pub fn unit_vector(v: anytype) @TypeOf(v) {
     return v / @as(@TypeOf(v), @splat(@as(vsize(v), vlen(v))));
+}
+
+pub fn dot_vector(v1: Vec3, v2: Vec3) f64 {
+    const a = v1[0] * v2[0];
+    const b = v1[1] * v2[1];
+    const c = v1[2] * v2[2];
+    return a + b + c;
 }
