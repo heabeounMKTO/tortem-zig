5124b0ea4a18bfe62bb45cdfa4449d53be8df8fc
heabeounMKTO <heabeoun69zin@gmail.com>
2024-08-25 09:04:31 +0700

    attempt at fixing arraylist
    i am playing gta 5 for now

--- src/hitable.zig
+++ src/hitable.zig
@@ -2,30 +2,32 @@ const Vec3 = @import("vec.zig").Vec3;
 const Ray = @import("ray.zig").Ray;
 const vec = @import("vec.zig");
 const sphere = @import("sphere.zig");
-
+const std = @import("std");
 
 // we only have spheres for now kied
 pub const HitableList = struct {
-    objects: [] sphere.Sphere,  
+    objects: std.ArrayList(sphere.Sphere),
+    allocator: std.mem.Allocator,
     const Self = @This();
-    
+
+    pub fn deinit(self: *Self) void {
+        self.objects.deinit();
+    }
+
     // initializes a world
-    pub fn create(object: sphere.Sphere) HitableList {
-        return HitableList {
-            .objects = [_]sphere.Sphere{object},
-        };
+    pub fn create(allocator: std.mem.Allocator) HitableList {
+        return HitableList{ .objects = std.ArrayList(sphere.Sphere).init(allocator), .allocator = allocator };
     }
 
     pub fn add(self: *HitableList, object: sphere.Sphere) !void {
-        self.objects.append(object);
+        try self.objects.append(object);
     }
-
 };
 
-pub fn check_world_hit(world: HitableList, r: Ray , ray_tmin: f64, ray_tmax: f64) HitRecord {
+pub fn check_world_hit(world: HitableList, r: Ray, ray_tmin: f64, ray_tmax: f64) HitRecord {
     var hit_rec = HitRecord.init();
     for (world.objects) |*object| {
-        var closest_so_far: f64 = 0.0; 
+        var closest_so_far: f64 = 0.0;
         const hit_kor_mex = object.hit(r, ray_tmin, ray_tmax);
         closest_so_far = hit_kor_mex.t;
         if (hit_kor_mex.is_hit) {
@@ -43,25 +45,13 @@ pub const HitRecord = struct {
     is_hit: bool,
     const Self = @This();
     pub fn no_hits() HitRecord {
-        return HitRecord {
-            .point = vec.Vec3 {0.0, 0.0, 0.0},
-            .normal = vec.Vec3 {0.0 , 0.0, 0.0},
-            .t = 0.0,
-            .front_face = false,
-            .is_hit = false
-        };
+        return HitRecord{ .point = vec.Vec3{ 0.0, 0.0, 0.0 }, .normal = vec.Vec3{ 0.0, 0.0, 0.0 }, .t = 0.0, .front_face = false, .is_hit = false };
     }
     pub fn init() HitRecord {
-        return HitRecord {
-            .point = vec.Vec3 {0.0, 0.0, 0.0},
-            .normal = vec.Vec3 {0.0 , 0.0, 0.0},
-            .t = 0.0,
-            .front_face = false,
-            .is_hit = false
-        };
+        return HitRecord{ .point = vec.Vec3{ 0.0, 0.0, 0.0 }, .normal = vec.Vec3{ 0.0, 0.0, 0.0 }, .t = 0.0, .front_face = false, .is_hit = false };
     }
     fn set_face_normal(self: *HitRecord, r: Ray, outward_normal: Vec3) !void {
-        self.front_face = vec.dot_vector(r.direction, outward_normal) < 0.0; 
+        self.front_face = vec.dot_vector(r.direction, outward_normal) < 0.0;
         if (self.front_face) {
             self.normal = outward_normal;
         } else {
@@ -69,4 +59,3 @@ pub const HitRecord = struct {
         }
     }
 };
-
--- src/main.zig
+++ src/main.zig
@@ -11,37 +11,42 @@ const p3 = vec.Point3;
 const v3 = vec.Vec3;
 
 pub fn main() !void {
+    var gpalloc = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpalloc.deinit();
+    const allocator = gpalloc.allocator();
+
     const image_width: u32 = 500;
     const image_height: u32 = 250;
-    const aspect_ratio: f64 = @as(f64, image_width)/@as(f64, image_height);
+    const aspect_ratio: f64 = @as(f64, image_width) / @as(f64, image_height);
     const viewport_height = 2.0;
     const viewport_width = aspect_ratio * viewport_height;
 
     const focal_length = 1.0;
-    const origin = p3{0.0,0.0, 0.0};
+    const origin = p3{ 0.0, 0.0, 0.0 };
 
-    const viewport_u = v3{viewport_width, 0.0,0.0};
-    const viewport_v = v3{0.0, -viewport_height, 0.0}; // we reverse da Y axis 
-    
-    const pixel_delta_u = viewport_u / @as(v3, @splat(@as(f64, @floatFromInt(image_width)))); 
+    const viewport_u = v3{ viewport_width, 0.0, 0.0 };
+    const viewport_v = v3{ 0.0, -viewport_height, 0.0 }; // we reverse da Y axis
+
+    const pixel_delta_u = viewport_u / @as(v3, @splat(@as(f64, @floatFromInt(image_width))));
     const pixel_delta_v = viewport_v / @as(v3, @splat(@as(f64, @floatFromInt(image_height))));
 
-    const viewport_top_left = origin - v3{0.0, 0.0, focal_length} - viewport_u / @as(v3, @splat(@as(f64, 2.0))) - viewport_v/@as(v3,@splat(@as(f64,2.0)));
-    const world = HitableList.create(Sphere { .center = vec.Vec3 { 0.0, 0.0, -1.0} , .radius = 0.5});
+    const viewport_top_left = origin - v3{ 0.0, 0.0, focal_length } - viewport_u / @as(v3, @splat(@as(f64, 2.0))) - viewport_v / @as(v3, @splat(@as(f64, 2.0)));
 
-    const pixel_00_loc = viewport_top_left + @as(v3,@splat(@as(f64,0.5))) * (pixel_delta_u + pixel_delta_v);
-    try print.print("P3\n{} {}\n255\n", .{image_width, image_height});
-    var j:i32 = 0;
-    while (j < image_height) : (j += 1){
-        var  i: i32 = 0;
-        while( i < image_width ): (i += 1) {
+    var world = HitableList.create(allocator);
+    defer world.deinit();
+
+    try world.add(Sphere{ .center = vec.Vec3{ 0.0, 0.0, -1.0 }, .radius = 0.5 });
+
+    const pixel_00_loc = viewport_top_left + @as(v3, @splat(@as(f64, 0.5))) * (pixel_delta_u + pixel_delta_v);
+    try print.print("P3\n{} {}\n255\n", .{ image_width, image_height });
+    var j: i32 = 0;
+    while (j < image_height) : (j += 1) {
+        var i: i32 = 0;
+        while (i < image_width) : (i += 1) {
             const pixel_center = pixel_00_loc + (@as(v3, @splat(@as(f64, @floatFromInt(i)))) * pixel_delta_u) + (@as(v3, @splat(@as(f64, @floatFromInt(j)))) * pixel_delta_v);
-            const r: ray.Ray = ray.Ray {
-                .origin=origin,
-                .direction=pixel_center
-            };
+            const r: ray.Ray = ray.Ray{ .origin = origin, .direction = pixel_center };
             const pixel_color = color.pointToColor(color.rayColor_(r, world));
-            try print.print("{} {} {}\n", .{pixel_color[0], pixel_color[1], pixel_color[2]});
+            try print.print("{} {} {}\n", .{ pixel_color[0], pixel_color[1], pixel_color[2] });
         }
     }
-} 
+}
